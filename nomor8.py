883613fc0862e2cbcc86d901988a2701c4c4b9f1
henray404 <henryalifian13@gmail.com>
2024-09-01 08:47:14 +0700

    Update nomor8.py

--- nomor8.py
+++ nomor8.py
@@ -1,4 +1,4 @@
-person = int(input("input total person"))
+person = int(input("input total person per day "))
 
 new_toilet = 2  # liter per flush
 person_toilet = 3  # person per toilet
@@ -7,8 +7,10 @@ avg_flush = 14  # flush per day
 cost_intall = 150  # dollar per install
 
 
-toilet = person / person_toilet  # count how many toilet
-
+toilet = int(person / person_toilet)  # count how many toilet
+toilet_cost = toilet * cost_intall
 magnitude = (toilet * avg_flush * old_toilet) - (toilet *
                                                  avg_flush * new_toilet)  # calculate the magnitude
-print(magnitude)
+
+print(f"the cost for intalling new toilet is {toilet_cost}")
+print(f"the estimate fot the magnitude is {magnitude} liter/day ")
