763e9d2415a7d8b94bc2b5f4aa47cbb29f59cf40
henray404 <henryalifian13@gmail.com>
2024-09-01 08:46:26 +0700

    Update nomor2.py

--- nomor2.py
+++ nomor2.py
@@ -1,6 +1,6 @@
 
-flow = float(input("enter flow rate"))   # m^3/s
-height = float(input("enter height"))  # meter
+flow = float(input("enter flow rate "))   # m^3/s
+height = float(input("enter height "))  # meter
 
 efficient = float(0.90)  # persentase tenaga listrik yang didapatkan dari usaha
 gravity = float(9.80)  # m/sec^2
@@ -12,4 +12,5 @@ works = water_mass * gravity * height  # nilai usaha
 megawatt = (works * efficient) / 10 ** 6  # mendapatkan nilai megawatt
 
 
-print(megawatt)
+print(f" amount of power that  produced is {megawatt} MW13")
+
